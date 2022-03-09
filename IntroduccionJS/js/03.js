43d004d558e05399b78854c9e31066e6abd65cf9
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-03-09 20:46:55 -0300

    new file

---
+++ IntroduccionJS/js/03.js
@@ -0,0 +1,12 @@
+//OBJETOS 
+const nombreProducto = "monitor 32 pulgadas";
+const precio=300;
+const disponible=true;
+
+const producto2={
+    nombre: nombreProducto,
+    precio: precio,
+    estado: disponible
+}
+
+console.log(producto2)
